0358de743360bf955b30341e5a6fd9e7f8b011aa
numichael <numichael.dev@gmail.com>
2021-09-06 00:08:16 +0100

    pushing general arithmetic calculator to github

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
+            string symbol = "";
+
+            Console.WriteLine("Welcome to the General Arithmetic Calculator");
+            Console.WriteLine();
+
+            Console.Write("Enter first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter an operation symbol:");
+            symbol = Console.ReadLine();
+
+            Console.Write("Enter second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            if(symbol == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if(symbol == "-")
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if(symbol == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if(symbol == "/")
+            {
+                result = firstNumber / secondNumber;
+            }
+            Console.WriteLine("The result is: " + result);
+        }
+
+    }
+}
